4bdde367a4af52c3566f87d155466cc5a4c8801b
adamfroio <afroio@gmail.com>
2024-05-20 08:40:17 -0700

    Added functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Post from "./components/Post";
+import SelectedItem from "./components/SelectedItem";
+import Barry from "./otters/otter1.jpg";
+import Robin from "./otters/otter2.jpg";
+import Maurice from "./otters/otter3.jpg";
+import Lesley from "./otters/otter4.jpg";
+import Barbara from "./otters/otter5.jpg";
+
+const ottersArray = [
+	{ image: Barry, name: "Barry", id: 1 },
+	{ image: Robin, name: "Robin", id: 2 },
+	{ image: Maurice, name: "Maurice", id: 3 },
+	{ image: Lesley, name: "Lesley", id: 4 },
+	{ image: Barbara, name: "Barbara", id: 5 },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<div className="app-content">
+				<ul className="post-list">
+					{ottersArray.map((post) => (
+						<Post
+							key={post.id}
+							image={post.image}
+							name={post.name}
+						/>
+					))}
+				</ul>
+				<SelectedItem
+					image={ottersArray[0].image}
+					name={ottersArray[0].name}
+				/>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+	return (
+		<header className="footer-component">
+			<h3>Built by: Leroy Jenkins</h3>
+		</header>
+	);
+}
+
+export default Footer;
