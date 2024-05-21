1db81802dd839094006090e83cb682b9ea19ae08
adamfroio <afroio@gmail.com>
2024-05-21 15:45:52 -0700

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import "./App.css";
-import Footer from "./components/Footer.js";
-import Header from "./components/Header.js";
-import Home from "./components/Home.js";
-import { items } from "./items/index.js";
+import Footer from './components/Footer.js';
+import Header from './components/Header.js';
+import Home from './components/Home.js';
+import { items } from './items/data.js';
 
 function App() {
 	return (
 		<div>
-			<Header />
+			<Header title="Kode Klatsch" />
 			<Home items={items} />
 			<Footer />
 		</div>
