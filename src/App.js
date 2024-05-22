efe21bbd4997dfde8ea83a51fb39797f3552bfd3
adamfroio <afroio@gmail.com>
2024-05-21 17:18:13 -0700

    integrated with backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Footer from './components/Footer.js';
 import Header from './components/Header.js';
 import Home from './components/Home.js';
-import { items } from './items/data.js';
 
 function App() {
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('/api/items')
+			.then((result) => setItems(result.data))
+			.catch(console.error);
+	}, []);
+
 	return (
 		<div>
 			<Header title="Kode Klatsch" />
